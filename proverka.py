143a296dede80e4287f29a549243595c06964723
Zafar Tagiyev <contorax@yandex.com>
2019-02-10 20:34:30 -0700

    Update proverka.py

--- proverka.py
+++ proverka.py
@@ -1,13 +1,42 @@
-def sum (zzz, ddd):
-    """What percentage of x is y"""
-    result = zzz+ddd
-    #print("first sum of " + str(zzz) + " and " + str(ddd) + " is " + str (result))
-    return result
+# # def sum(zzz, ddd):
+# #     """What percentage of x is y"""
+# #     result = zzz + ddd
+# #     # print("first sum of " + str(zzz) + " and " + str(ddd) + " is " + str (result))
+# #     return result
 
 
-def print_sum(x, y):
-    """Print what percentage of x is y"""
-    print("second sum of " + str(x) + " and " + str(y) + " is " + str(sum (x, y)))
+# # def print_sum(x, y):
+# #     """Print what percentage of x is y"""
+# #     print(" second sum of " + str(x) + " and " + str(y) + " is " + str(sum(x, y)))
 
 
-print_sum(3,20)
+# # print_sum(3, 20)
+
+# import os
+# import glob
+# os.chdir("/Users/zafar/Documents/from my Asus")
+# for file in glob.glob("*.txt"):
+#     print (file)
+
+# def sum(zzz, ddd):
+#     """What percentage of x is y"""
+#     result = zzz + ddd
+#     # print("first sum of " + str(zzz) + " and " + str(ddd) + " is " + str (result))
+#     return result
+
+
+# def print_sum(x, y):
+#     """Print what percentage of x is y"""
+#     print(" second sum of " + str(x) + " and " + str(y) + " is " + str(sum(x, y)))
+
+
+# print_sum(3, 20)
+
+with open('/Users/zafar/Documents/eng_test.txt', 'r') as f:
+    f_contents = f.read()
+    print (f_contents)
+
+# f = open('/Users/zafar/Documents/from my Asus/passwordALL12.txt', 'r')
+# print (f.name)
+
+# f.close
